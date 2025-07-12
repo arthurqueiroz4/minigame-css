f3a131ad59f6d8ec30239c81bb3baff5a1aeb8a3
Jorge Vitor <jorgevitorlima8@gmail.com>
2025-07-12 01:26:34 -0300

    add BHopMinigame

---
+++ Minigame/Minigames/BHopMinigame.cs
@@ -0,0 +1,38 @@
+using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Modules.Entities;
+using Minigame.Utils;
+
+namespace Minigame.Minigames;
+
+public class BHopMinigame : IMinigame
+{
+    public BHopMinigame(BasePlugin plugin)
+    {
+        Plugin = plugin;
+    }
+
+    public BasePlugin Plugin { get; }
+    public string Name => "Bunny Hop";
+
+    public void Register(List<CCSPlayerController>? players = null)
+    {
+        Helper.RunCommand("sv_autobunnyhopping", "1");
+        Helper.RunCommand("sv_enablebunnyhopping", "1");
+        Helper.RunCommand("sv_airaccelerate", "2000");
+        Helper.RunCommand("sv_staminajumpcost", "0");
+        Helper.RunCommand("sv_staminalandcost", "0");
+        Helper.RunCommand("sv_staminarecoveryrate", "0");
+
+    }
+
+    public void Unregister()
+    {
+        Helper.RunCommand("sv_autobunnyhopping", "0");
+        Helper.RunCommand("sv_enablebunnyhopping", "0");
+        Helper.RunCommand("sv_airaccelerate", "12");
+        Helper.RunCommand("sv_staminajumpcost", "0.080");
+        Helper.RunCommand("sv_staminalandcost", "0.050");
+        Helper.RunCommand("sv_staminarecoveryrate", "60");
+    }
+}
