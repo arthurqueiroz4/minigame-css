2177c524389bde4f7a8b2832ceea084afe51772e
Jorge Vitor <jorgevitorlima8@gmail.com>
2025-07-23 20:29:24 -0300

    Add complete invisible minigames

--- Minigame/Minigames/InvisibleMinigame.cs
+++ Minigame/Minigames/InvisibleMinigame.cs
@@ -1,6 +1,9 @@
 using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Core;
 using System.Drawing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Minigame.Minigames;
 
@@ -13,9 +16,20 @@ public class InvisibleMinigame : IMinigame
     public BasePlugin Plugin { get; }
     public string Name => "Invisible Players";
 
+    private List<float> invisTimes = new();
+    private float roundTime = 0f;
+    private bool isInvisible = false;
+    private float invisEndTime = 0f;
+    private Random rng = new();
+
     public void Register(List<CCSPlayerController>? players = null)
     {
         Plugin.RegisterListener<Listeners.OnTick>(OnTickInvisible);
+
+        invisTimes = Enumerable.Range(0, 3).Select(_ => (float)rng.Next(10, 115)).OrderBy(x => x).ToList();
+        roundTime = 0f;
+        isInvisible = false;
+        invisEndTime = 0f;
     }
 
     public void Unregister()
@@ -30,10 +44,29 @@ public class InvisibleMinigame : IMinigame
 
     private void OnTickInvisible()
     {
-        var players = Utilities.GetPlayers();
-        foreach (var player in players)
+        roundTime += Server.TickInterval;
+
+        if (!isInvisible && invisTimes.Count > 0 && roundTime >= invisTimes[0])
         {
-            ChangePlayerVisible(player, 0);
+            isInvisible = true;
+            invisEndTime = roundTime + 10f;
+            invisTimes.RemoveAt(0);
+            Server.PrintToChatAll("\x04[Minigame] \x01 All players are now \x04INVISIBLE\x01 for 10 seconds!");
+        }
+
+        if (isInvisible)
+        {
+            var players = Utilities.GetPlayers();
+            foreach (var player in players)
+                ChangePlayerVisible(player, 0);
+
+            if (roundTime >= invisEndTime)
+            {
+                isInvisible = false;
+                foreach (var player in players)
+                    ChangePlayerVisible(player, 255);
+                Server.PrintToChatAll("\x04[Minigame] \x01 All players are \x02NO LONGER\x01 invisible!");
+            }
         }
     }
 
@@ -44,7 +77,6 @@ public class InvisibleMinigame : IMinigame
         if (playerPawn == null)
             return;
 
-        // Torna o jogador invisível (0) ou visível (255)
         playerPawn.Render = Color.FromArgb(count, 255, 255, 255);
         Utilities.SetStateChanged(playerPawn, "CBaseModelEntity", "m_clrRender");
 
