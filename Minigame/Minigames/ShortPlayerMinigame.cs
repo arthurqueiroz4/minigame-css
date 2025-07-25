c7b29e69485d541f2e576cde381edae9200432d6
Jorge Vitor <jorgevitorlima8@gmail.com>
2025-07-25 02:35:12 -0300

    Add Big and Short PlayersMinigames

---
+++ Minigame/Minigames/ShortPlayerMinigame.cs
@@ -0,0 +1,80 @@
+using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Minigame.Minigames;
+
+public class ShortPlayerMinigame : IMinigame
+{
+    public ShortPlayerMinigame(BasePlugin plugin)
+    {
+        Plugin = plugin;
+    }
+    public BasePlugin Plugin { get; }
+    public string Name => "Short Players";
+
+    private List<CCSPlayerPawn> playersWithChangedModelSize = new();
+    private float shortSize = 0.7f;
+
+    public void Register(List<CCSPlayerController>? players = null)
+    {
+        var targetPlayers = players ?? Utilities.GetPlayers();
+        foreach (var player in targetPlayers)
+        {
+            ChangePlayerSize(player);
+        }
+    }
+
+    public void Unregister()
+    {
+        ResetAllPlayerSizes();
+    }
+
+    private void ChangePlayerSize(CCSPlayerController? player)
+    {
+        if (player == null || player.PlayerPawn == null || !player.PlayerPawn.IsValid || player.PlayerPawn.Value == null)
+            return;
+        var playerPawn = player.PlayerPawn.Value;
+        if (playersWithChangedModelSize.Contains(playerPawn))
+            return;
+        var playerSceneNode = playerPawn.CBodyComponent?.SceneNode;
+        if (playerSceneNode == null)
+            return;
+        playerSceneNode.GetSkeletonInstance().Scale = shortSize;
+        playerPawn.AcceptInput("SetScale", null, null, shortSize.ToString());
+        Server.NextFrame(() =>
+        {
+            if (playerPawn == null) return;
+            Utilities.SetStateChanged(playerPawn, "CBaseEntity", "m_CBodyComponent");
+        });
+        playersWithChangedModelSize.Add(playerPawn);
+    }
+
+    private void ResetAllPlayerSizes()
+    {
+        var copy = new List<CCSPlayerPawn>(playersWithChangedModelSize);
+        foreach (var playerPawn in copy)
+        {
+            try
+            {
+                if (playerPawn == null) continue;
+                var playerSceneNode = playerPawn.CBodyComponent?.SceneNode;
+                if (playerSceneNode == null) continue;
+                playerSceneNode.GetSkeletonInstance().Scale = 1.0f;
+                playerPawn.AcceptInput("SetScale", null, null, "1.0");
+                Server.NextFrame(() =>
+                {
+                    if (playerPawn == null) return;
+                    Utilities.SetStateChanged(playerPawn, "CBaseEntity", "m_CBodyComponent");
+                });
+            }
+            catch
+            {
+                
+            }
+        }
+        playersWithChangedModelSize.Clear();
+    }
+} 
